20e17a3af573337e4897ddaaa4834cc5b638d9c9
wueato03 <alvaro.torres@entelgy.com>
2017-10-13 13:21:51 +0200

    starting to work

--- hipoteca.py
+++ hipoteca.py
@@ -6,11 +6,13 @@ DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
- 
+
+
 class ReusableForm(Form):
     cantidad = TextField('Cantidad:', validators=[validators.required()])
     plazo = TextField('Plazo:', validators=[validators.required()])
     interes = TextField('Interés:', validators=[validators.required()])
+
  
 @app.route("/", methods=['GET', 'POST'])
 def hello():
@@ -18,16 +20,41 @@ def hello():
  
     print(form.errors)
     if request.method == 'POST':
-        cantidad=request.form['cantidad']
+        cantidad = int(request.form['cantidad'])
+        plazo = int(request.form['plazo'])
+        interes = float(request.form['interes'])
         print(cantidad)
  
         if form.validate():
             # Save the comment here.
-            flash('Hello ' + cantidad)
+            cuota = str(calc(cantidad, interes, plazo))
+            flash('Tu cuota mensual es: ' + cuota)
         else:
             flash('All the form fields are required. ')
  
     return render_template('hipoteca.html', form=form)
+
+
+def calc(prestado, tae, plazoA):
+    print(prestado, tae, plazoA)
+
+    taeM = tae / 1200
+    plazo = plazoA * 12
+
+    restante = prestado
+    interes = 0
+    cuota = 0
+
+    while restante > 0:
+        restante = prestado
+        interes = 0
+        cuota += 0.1
+        for _ in range(plazo):
+            interesM = restante * taeM
+            interes += interesM
+            restante -= (cuota - interesM)
+    print(restante, interes, cuota)
+    return cuota
  
 if __name__ == "__main__":
     app.run()
